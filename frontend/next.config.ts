9a603aba75e0a685fc52216115384bedb5bea732
Elif Alim <elifalim1506@gmail.com>
2025-07-21 00:49:04 +0300

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,5 @@
-import type { NextConfig } from "next";
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    esmExternals: false, // <-- önemli
-  },
-  images: {
-    domains: ['localhost'],
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
